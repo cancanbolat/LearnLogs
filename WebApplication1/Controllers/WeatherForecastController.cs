534534695f86cc5b61c929b1323e10d127003887
cancanbolat <can.canbolat12@gmail.com>
2022-05-08 12:42:17 +0300

    first commit

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            // Loglama
+            // appSettings.Development içinden log ayarları yapılabilir. orası Program.cs'i ezer.
+            // log seviyesine göre aşağıdaki logları basar.
+            _logger.LogTrace("LogTrace -> Get method is called."); //min log seviyesi Trace olsa burası da basılır.
+            _logger.LogDebug("LogDebug -> Get method is called.");
+            _logger.LogInformation("LogInformation -> Get method is called.");
+            _logger.LogWarning("LogWarning -> Get method is called.");
+            _logger.LogError("LogError -> Get method is called.");
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ WebApplication1/Logging/MyCustomLoggerFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Logging
+{
+    public class MyCustomLoggerFactory : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new MyCustomLogger();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+
+    public class MyCustomLogger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            // log seviyesi kontrolü
+            return true;
+        }
+
+        // log mesajı burada ayarlanıyor
+        // burada konsola mı dosyaya mı veya başka bir yere basıldığını ayarlayabilirsin.
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            string logMsg = formatter(state, exception);
+
+            logMsg = $"[{DateTime.Now:dd.MM.yyyy HH:mm:ss}] -> {logMsg}";
+
+            Console.WriteLine(logMsg);
+        }
+    }
+}
